58b2d77c5908a0294437e6e94c885fd2ff1d580e
kanizfatema10 <knzkoly@gmail.com>
2023-03-22 23:00:02 +0600

    login test complete

--- src/test/java/model/TestData.java
+++ src/test/java/model/TestData.java
@@ -11,8 +11,12 @@ public class TestData {
     public Object[][] getData() {
     // return a two-dimensional object array with test data
     return new Object[][] { 
+        {"",""},
+        {"test","1234"},
         { "test", credentials.loginPasswordInputText }, 
         { credentials.loginUserNameInputText, "1234" }, 
+        { "", credentials.loginPasswordInputText }, 
+        { credentials.loginUserNameInputText, "" }, 
         { credentials.loginUserNameInputText, credentials.loginPasswordInputText} 
     };
 }
--- src/test/java/tests/MainLoginTest.java
+++ src/test/java/tests/MainLoginTest.java
@@ -35,20 +35,44 @@ public class MainLoginTest extends Base{
         mainLoginPage.clickOnLogin();
         UtilMethods.waitForSeconds(3);
         
-        if (userName == "standard_user" && password == "secret_sauce") {
-            WebElement findAppLogoNameClass = driver.findElement(InventoryLocators.appLogoName);
-            String actualAppLogoName = findAppLogoNameClass.getText();
-            String expectedAppLogoName = "Swag Labs";
+        checkAssertion(userName, password);
+    }
 
-            Assert.assertEquals(actualAppLogoName, expectedAppLogoName);
-            
-        } else {
-            WebElement findH3HeaderText = driver.findElement(MainLoginLocators.errorMessageForInvalidCredential);
-            String actualErrorMessage = findH3HeaderText.getText();
-            String expectedErrorMessage = "Epic sadface: Username and password do not match any user in this service";
-            
-            Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
-        }
+    private void checkAssertion(String userName, String password){
+        WebElement webElementToCheck;
+        String actualString; 
+        String expectedString; 
+
+        switch(userName +"_+_"+ password) {
+            case "standard_user_+_secret_sauce":
+                System.out.println(".........................all right.............");
+                webElementToCheck = driver.findElement(InventoryLocators.appLogoName);
+                actualString = webElementToCheck.getText();
+                expectedString = "Swag Labs";
+                Assert.assertEquals(actualString, expectedString);
+                break;
+            case "_+_":
+            case "_+_secret_sauce":
+                System.out.println(".........................Epic sadface: Username is required.............");
+                webElementToCheck = driver.findElement(MainLoginLocators.errorMessageForEmptyUserName);
+                actualString = webElementToCheck.getText();
+                expectedString = "Epic sadface: Username is required";          
+                Assert.assertEquals(actualString, expectedString);
+                break;
+            case "standard_user_+_":
+                System.out.println(".........................Epic sadface: Password is required.............");
+                webElementToCheck = driver.findElement(MainLoginLocators.errorMessageForEmptyPassword);
+                actualString = webElementToCheck.getText();
+                expectedString = "Epic sadface: Password is required";          
+                Assert.assertEquals(actualString, expectedString);
+                break;     
+            default:
+                System.out.println(".........................wrong.............");
+                webElementToCheck = driver.findElement(MainLoginLocators.errorMessageForWrongCredential);
+                actualString = webElementToCheck.getText();
+                expectedString = "Epic sadface: Username and password do not match any user in this service";          
+                Assert.assertEquals(actualString, expectedString);
+          }
     }
 }
     
