20c74f7552b1473f23a2e13af49771c361d6c9a3
kanizfatema10 <knzkoly@gmail.com>
2023-03-25 22:15:42 +0600

    located necessary locators

---
+++ src/test/java/model/CheckoutLocators.java
@@ -0,0 +1,26 @@
+package model;
+
+import org.openqa.selenium.By;
+
+public class CheckoutLocators {
+    public static By yourCheckoutInfo = By.xpath("//span[@class='title'] [contains(text(),'Checkout: Your Information')]");
+    public static By firstName = By.id("first-name");
+    public static By lastName = By.id("last-name");
+    public static By postalCode = By.id("postal-code");
+    public static By continueCheckoutButton = By.id("continue");
+    public static By cancelCheckoutButton = By.id("cancel");
+    public static By checkoutCompleteTitle = By.xpath("//span[@class='title'] [contains(text(),'Checkout: Complete!')]");
+    public static By orderConfirmationMessage = By.xpath("//H2[@class='complete-header'] [contains(text(),'Thank you for your order!')]");   
+    public static By errorMessageForEmptyFirstName = By.xpath("//h3[contains(text(),'Error: First Name is required')]");
+    public static By errorMessageForEmptyLastName = By.xpath("//h3[contains(text(),'Error: Last Name is required')]");
+    public static By errorMessageForEmptyPostalCode = By.xpath("//h3[contains(text(),'Error: Postal Code is required')]");
+    public static By checkoutOverviewTitle = By.xpath("//span[@class='title'] [contains(text(),'Checkout: Overview')]");
+    public static By priceOfFirstItemFromCartList = By.xpath("(//div[@class='cart_list']//div[@class='inventory_item_price'])[1]/text()[2]");
+    public static By priceOfSecondItemFromCartList = By.xpath("(//div[@class='cart_list']//div[@class='inventory_item_price'])[2]/text()[2]");
+    public static By itemTotal = By.xpath("//div[@class='summary_subtotal_label']/text()[2]");
+    public static By taxAmount = By.xpath("//div[@class='summary_tax_label']/text()[2]");
+    public static By totalPayableAmount = By.xpath("//div[@class='summary_info_label summary_total_label']/text()[2]");
+    public static By finishButton = By.id("finish");
+    public static By cancelButton = By.id("finish");
+
+}
--- src/test/java/model/InventoryLocators.java
+++ src/test/java/model/InventoryLocators.java
@@ -4,4 +4,29 @@ import org.openqa.selenium.By;
 
 public class InventoryLocators {
     public static By appLogoName = By.cssSelector(".app_logo");
+    public static By burgerMenuIcon = By.id("react-burger-menu-btn");
+    public static By aboutMenuText = By.id("about_sidebar_link");
+    public static By logOutMenuText = By.id("logout_sidebar_link");
+    public static By burgerMenuCrossIcon = By.id("react-burger-cross-btn");
+    public static By inventoryPageTitle = By.xpath("    //span[@class='title'] [contains(text(),'Products')]");
+    public static By productFilter = By.xpath("//span[@class='active_option'] ");
+    public static By aTozFilterOption = By.xpath("//select/option[contains(text(),'Name (A to Z)')]");
+    public static By zToaFilterOption = By.xpath("//select/option[contains(text(),'Name (Z to A)')]");
+    public static By priceLowToHighFilterOption = By.xpath("//select/option[contains(text(),'Price (low to high)')]");
+    public static By priceHighToLowFilterOption = By.xpath("//select/option[contains(text(),'Price (high to low)')]");
+    public static By firstItemLink = By.xpath("//div[@class='inventory_item_name'][contains(text(),'Sauce Labs Backpack')]");
+    public static By itemNameInDetailPage = By.xpath("//div[@class='inventory_details_name large_size'] [contains(text(),'Sauce Labs Backpack')]");
+    public static By backToProductButton = By.id("back-to-products");
+    public static By addToCardSauceLabsBackpack = By.id("add-to-cart-sauce-labs-backpack");
+    public static By removeSauceLabsBackpack = By.id("remove-sauce-labs-backpack");
+    public static By shoppingCardLink = By.id("shopping_cart_link");
+    public static By shoppingCardBadge = By.id("shopping_cart_badge");
+    public static By yourCartTitle = By.xpath("//span[@class='title'] [contains(text(),'Your Cart')]");
+    public static By continueShoppingButton = By.id("continue-shopping");
+    public static By checkoutButton = By.id("checkout");
+
+
+
+
+
 }
